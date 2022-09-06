5f26c1c218ebdd60c2fff3d75808cc24d96a989a
abdullatef trefi <abdullatef.trefi98@gmail.com>
2022-09-06 23:23:43 +0300

    fix the server was not starting

--- ecommerce/src/server.ts
+++ ecommerce/src/server.ts
@@ -1,8 +1,20 @@
 import express, { Request, Response } from 'express';
 import http from 'http2';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 
-const server = http.createSecureServer({}, (req, res) => {
-  app(req as unknown as Request, res as unknown as Response);
-});
+http
+  .createSecureServer(
+    {
+      key: fs.readFileSync(path.resolve(__dirname, '../keys/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../keys/cert.pem')),
+    },
+    (req, res) => {
+      app(req as unknown as Request, res as unknown as Response);
+    },
+  )
+  .listen(process.env.PORT || 5000, () => {
+    console.log('started');
+  });
